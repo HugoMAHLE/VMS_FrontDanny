fc145b1f47632553ac1e982d1e0cde3780db7e32
TellezJr24 <daniel.a.tellez@mahle.com>
2025-02-18 13:33:14 -0700

    Main lobby
    Se agregro que el main lobby pueda ver todas las visitas de las plantas, asi mismo se le agrego una forma mejor de que aparezcan las cartas de las visitas presionando un boton

--- src/app/reception/reception.component.ts
+++ src/app/reception/reception.component.ts
@@ -4,16 +4,20 @@ import { MatCardModule } from '@angular/material/card';
 import { Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { title } from 'process';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-reception',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule, MatToolbarModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatToolbarModule, NgIf, NgFor],
   templateUrl: './reception.component.html',
   styleUrl: './reception.component.css'
 })
 export class ReceptionComponent {
 router = inject(Router);
+
+showCards = false;
 
 logOff() {
   console.log("sesion terminada");
@@ -24,6 +28,25 @@ logOff() {
 
 navConfirmCaseta(){
   this.router.navigate(["/security/confirm"]);
+}
+
+cards1 : string[] = [];
+cards3 : string[] = [];
+cards4 : string[] = [];
+
+AddCard(){
+  const newCard = `Carta ${this.cards1.length + 1}`;
+  this.cards1.push(newCard);
+}
+
+AddCard3(){
+  const newCard = `Carta ${this.cards3.length + 1}`;
+  this.cards3.push(newCard);
+}
+
+AddCard4(){
+  const newCard = `Carta ${this.cards4.length + 1}`;
+  this.cards4.push(newCard);
 }
 
 }
